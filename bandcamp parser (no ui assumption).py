eb549883fbe3e011e0cd18d0168c0d77d2a3c94d
eating-coleslaw <hanzidane@hotmail.com>
2015-02-14 14:58:39 -0600

    Streamlined ui, updated cleaner regex, updated main script, started sql implementation.

--- bandcamp parser (no ui assumption).py
+++ bandcamp parser (no ui assumption).py
@@ -25,13 +25,13 @@ import itertools  #count function is convenient iterator
     
 
 
-SOURCE_URL = "spazzkid.bandcamp.com"  #will become main program input
+SOURCE_URL = "https://mishkanyc.bandcamp.com/music"  #will become main program input
 URL_QUEUE = deque([])
 TAG_QUEUE = deque([])
 OPENER = urllib2.build_opener()
 OPENER.addheaders = [('User-agent', 'Mozilla/5.0')]
-MAX_TAG_PAGES = 1
-MAX_ALBUMS = 5
+MAX_TAG_PAGES = 2
+MAX_ALBUMS = 10
 SOURCE_TAGS = []
 
 def main():
@@ -41,37 +41,17 @@ def main():
     SOURCE_TAGS = []
     pass
 
-
-def prepare_source_band_url(url):
-    """Cleans the user-input source url and turns it into a music page url."""
-    # check to ensure bandcamp.com link
-    bandcamp_check = re.compile('(.*)(bandcamp\.com)(.*)$')
-    if bandcamp_check.match(url) != None:
-        clean_url = check_url_protocol(get_clean_band_url(url))
-        return make_music_page_url(clean_url)
-    else:
-        print '%s is not a bandcamp.com URL.' % url
-
 def make_music_page_url(url):
+    """Converts band url into their music page."""
     if re.compile('/music').match(url) == None:
         return url + '/music'
     else:
         return url
-            
-def check_url_protocol(url):
-    """Ensures a given url has the http:// protocl."""
-    if re.compile('http://').match(url) == None:
-        url = 'http://' + url
-        return url
-    else:
-        return url
         
-def get_clean_band_url(url):
-    """Convert album/track url to base band url."""
-    cleaner = re.compile('(/([a-z]{5})/)(.*)$')
+def clean_url(url):
+    """Converts album/track url to base band url."""
+    cleaner = re.compile('(/(album|track|music|releases))(?![a-z])(/?)((.*)?)')
     cleaned_band_url = re.sub(cleaner, "", url)
-    print url
-    print cleaned_band_url
     return cleaned_band_url
     
 def make_soup(url):
@@ -90,33 +70,52 @@ def scrape_html(url):
     try:
         response = OPENER.open(url)
         if response.code == 200:
-            #print "Scraping %s" % url
+            print "Scraping %s" % url
             html = response.read()
         else:
             print "Invalid URL: %s \n" % url
-    except urllib2.HTTPError:
-        print "Failed to open %s \n" % url
+    except (urllib2.HTTPError, urllib2.URLError) as e:
+        print "Failed to open {}: {}".format(url, e)
     return html
 
-def parse_music_page(soup, url):
+def parse_music_page(url, artist):
     """Find/add all albumes on band's music page to URL_QUEUE."""
+    print "made it to parse_music_page"
+    soup = make_soup(url)
     album_count = 0
-    for tag in soup.find_all('li', class_='square '):
-        for link in tag.find_all('a'):
-            if album_count <= MAX_ALBUMS:
+    for album in soup.find_all('li', class_='square first-four'):
+        for link in album.find_all('a'):
+            print "found links in tag"
+            if artist.same_artist(SOURCE_ARTIST) or album_count <= MAX_ALBUMS:
                 album = link.get('href')
-                URL_QUEUE.append(check_url_protocol(url) + album)
+                clean = clean_url(url)
+                print url, clean
+                URL_QUEUE.append(clean + album)
                 album_count += 1
             else: 
+                print "done parsing music page"
+                return
+    for album in soup.find_all('li', class_='square '):
+        for link in album.find_all('a'):
+            print "found links in tag"
+            if artist.same_artist(SOURCE_ARTIST) or album_count <= MAX_ALBUMS:
+                album = link.get('href')
+                clean = clean_url(url)
+                print url, clean
+                URL_QUEUE.append(clean + album)
+                album_count += 1
+            else: 
+                print "done parsing music page"
                 return
 
-def get_album_tags(soup, artist):
+def get_album_tags(url, artist):
     """Find album's tags and add their urls and name to TAG_QUEUE."""
+    soup = make_soup(url)
     for tag in soup.find_all('a', class_='tag'):
         tag_url = tag.get('href')
         pretag_url = re.compile('(.*)(.bandcamp.com/tag/)')
         tag_string = re.sub(pretag_url, "", tag_url).replace("-", " ") # get the tag from the tag url
-        #print tag_string + " --- " + tag_url
+        print tag_string + " --- " + tag_url
         if artist.same_artist(SESSION_ARTISTS.get_source_artist()):
             artist.update_tags(tag_string)
             if [tag_url, tag_string] not in TAG_QUEUE:
@@ -130,27 +129,23 @@ def parse_tag_pages(url, tag):
     for album in soup.find_all("li", class_="item"):
         album_url = album.a.get("href")
         band_name = album.find(class_='itemsubtext').get_text()
-        #print band_name
-        band_url = get_clean_band_url(album_url)
+        print band_name
+        band_url = clean_url(album_url)
         SESSION_ARTISTS.add_artist(band_name, band_url)
-        #print band_name + " --- " + album_url
     try:
         next_page = soup.find(class_='nextprev next').a.get('href')
         if int(next_page[-1:]) <= MAX_TAG_PAGES:
             parse_tag_pages(url[ :-7] + next_page, tag)
         else:
-            pass
-            #print "\nFinished scraping %s: page limit reached.\n" % tag 
+            print "Finished scraping %s: page limit reached." % tag 
     except AttributeError:
-        pass
-        #print "\nFinished scraping %s: no more pages.\n" % tag
+        print "Finished scraping %s: no more pages." % tag
 
 def create_sql_db(db_name):
     print "Opening connection to %s." % (db_name + ".db")
     con = sqlite3.connect(db_name)
     sql = con.cursor()
     sql.execute("""CREATE TABLE IF NOT EXISTS artist_tags(
-         id INTEGER PRIMARY KEY,
          artist TEXT,
          url TEXT,
          tag TEXT,
@@ -246,31 +241,31 @@ class Session_Artists:
     def session_len(self):
         return len(self.artist_list)
         
-SOURCE_MUSIC_URL = prepare_source_band_url(SOURCE_URL)
-music_soup = make_soup(SOURCE_MUSIC_URL)
-parse_music_page(music_soup, SOURCE_URL)
-SOURCE_NAME = get_source_artist_name(SOURCE_MUSIC_URL)
-SESSION_ARTISTS = Session_Artists(SOURCE_NAME, SOURCE_MUSIC_URL)
-create_sql_db(SOURCE_NAME)
+class Timeout(Exception):
+    pass
 
-    #print tag
-    #SOURCE_TAGS.append(tag)
 
-print "Source Artist:", SESSION_ARTISTS.get_source_artist().get_name() + "\n"
+parse_music_page(SOURCE_URL, SOURCE_ARTIST)
+SOURCE_NAME = get_source_artist_name(SOURCE_URL)
+SESSION_ARTISTS = Session_Artists(SOURCE_NAME, SOURCE_URL)
+SOURCE_ARTIST = SESSION_ARTISTS.get_source_artist()
+
+con, sql = create_sql_db("JapBreak")
+
+
+print "Source Artist: %s\n" % SOURCE_NAME
 
 """Get tags from all source artist's albums."""
-
 print "Getting Source Artist tags.\n"
-while len(URL_QUEUE) > 0:
+while len(URL_QUEUE) > 0 :
     try:
-        album_soup = make_soup(URL_QUEUE.popleft())
-        get_album_tags(album_soup, SESSION_ARTISTS.get_source_artist())
-    except urllib2.URLError:
-        pass
-    
+        url = URL_QUEUE.popleft()
+        print url
+        get_album_tags(url, SOURCE_ARTIST)
+    except (urllib2.URLError, urllib2.HTTPError) as e:
+        print e
     
 """Get artists from all source tag pages."""
-
 print "Going through tag pages.\n"
 while len(TAG_QUEUE) > 0:
     try:
@@ -278,49 +273,40 @@ while len(TAG_QUEUE) > 0:
         tag_name = tag[1]
         tag_url = tag[0]
         parse_tag_pages(tag_url + '?page=1', tag_name)
-    except urllib2.URLError:
-        pass
-    
-    
+    except (urllib2.URLError, urllib2.HTTPError) as e:
+        print e
+       
 """Go through SESSION_ARTISTS' music pages -> albums -> tags."""
-
 print "Going through artist pages.\n"
 print "SESSION_ARTISTS:", len(SESSION_ARTISTS.get_artist_list())
 count = 0
 error_count = 0
 for artist in SESSION_ARTISTS.get_artist_list():
-    #print artist.get_name()
+    print artist.get_name(), artist.get_url()
     try:
-        music_soup = make_soup( make_music_page_url(artist.get_url()) )
-        parse_music_page(music_soup, artist.get_url())
+        parse_music_page(artist.get_url() + "/music", artist)
         while len(URL_QUEUE) > 0:
             count += 1
-            #print count
             url = URL_QUEUE.popleft()
-            #print url
-            album_soup = make_soup( url )
-            get_album_tags(album_soup, artist)
+            get_album_tags(url, artist)
             if ( count % 25 == 0):
                 print count
-        #print artist.get_name()
-    except urllib2.URLError:
+    except (urllib2.URLError, urllib2.HTTPError, TypeError) as e:
         error_count += 1
-        pass
+        print e, error_count
         
 print "\ndone looking at artists: %d errors\n" % error_count
 
-#for artist in SESSION_ARTISTS.get_artist_list():
-#print artist.get_url()
 print SESSION_ARTISTS.session_len()
-        #print artist.get_name() + ' --- ' + artist.get_tag_dictionary()[tag]
-
 print SESSION_ARTISTS.get_source_artist().get_tag_dictionary()
 print SOURCE_TAGS
 
 SESSION_HEAP = []
-#heapq.heapify(SESSION_HEAP)
 for artist in SESSION_ARTISTS.get_artist_list():
-    #print artist.get_unique_tag_count()
+    tag_dict = artist.get_tag_dictionary()
+    for tag in tag_dict:
+        data = (artist.get_name(), artist.get_url(), tag, tag_dict[tag])
+        sql.execute('INSERT INTO artist_tags VALUES (?,?,?,?)', data)
     heapq.heappush(SESSION_HEAP, ((100-artist.get_common_tag_count()), artist.get_name(), artist.get_url()))
 for a in range(0,11):
     print heapq.heappop(SESSION_HEAP)
--- test run results.py
+++ test run results.py
@@ -47,3 +47,36 @@ Feb 9, 2015 9:30 pm, spazzkid
     (82, u'Tom Ellard', 'http://severedheads.bandcamp.com')
     (83, u'Glowbug', 'http://glowbug.bandcamp.com')
     (85, u'Various Artists', 'http://urbanwavesrecords.com')
+    
+Feb 13, 2015 8:00 pm, spazzkid
+
+    {'pyschedelic': 1, 'indie': 6, 'kyoto': 1, 'abstract': 1, 'right now': 1, 'pop': 7, 'ep': 1, 'beats': 7, 'los angeles': 8, 'world': 1, 'electronic': 7, 'spazzkid': 2, 'experimental': 8}
+    ['beats', 'electronic', 'experimental', 'indie', 'pop', 'kyoto', 'los angeles', 'pyschedelic', 'abstract', 'spazzkid', 'world', 'ep', 'right now']
+    (49, u'spazzkid', 'http://spazzkid.bandcamp.com')
+    (76, u'Sea Oleena', 'http://lefserecords.bandcamp.com')
+    (79, u'Suokas', 'http://everything.fullofnothing.net')
+    (80, u'Professor Shyguy', 'http://professorshyguy.bandcamp.com')
+    (80, u'Thriftworks', 'http://jatlas.bandcamp.com')
+    (81, u'Kaytranada', 'http://huhwhatwhere.bandcamp.com')
+    (81, u'STEAK AU ZOO', 'http://steakauzoo.bandcamp.com')
+    (82, u'Julien Mier', 'http://cascaderecords.bandcamp.com')
+    (82, u'Tom Ellard', 'http://severedheads.bandcamp.com')
+    (83, u'Glowbug', 'http://glowbug.bandcamp.com')
+    (85, u'Blameful Isles', 'http://urbanwavesrecords.com')
+    
+Feb 14, 2013 11:00 pm, japanese breakfast
+
+    {'little big league': 1, 'indie': 2, 'female vocalist': 1, 'philadelphia': 3, 'pop': 2, 'bedroom pop': 1, 'experimental': 5}
+    ['experimental', 'bedroom pop', 'female vocalist', 'indie', 'pop', 'philadelphia', 'little big league']
+    (64, u'spazzkid', 'http://spazzkid.bandcamp.com')
+    (67, u'Sea Oleena', 'http://lefserecords.bandcamp.com')
+    (70, u'TV Girl', 'http://tvgirl.bandcamp.com')
+    (74, u'Professor Shyguy', 'http://professorshyguy.bandcamp.com')
+    (75, u'Alex G', 'http://sandy.bandcamp.com')
+    (76, u'Alex G', 'http://orchidtapes.bandcamp.com')
+    (76, u'Chelsea Wolfe', 'http://folkadelphia.bandcamp.com')
+    (77, u'Cyberbully Mom Club', 'http://cyberbullymomclub.bandcamp.com')
+    (77, u'HALA', 'http://halaband.bandcamp.com')
+    (78, u'DIOSQUE', 'http://plastilinarecords.bandcamp.com')
+    (78, u'Danielle Ate the Sandwich', 'http://danielleatethesandwich.bandcamp.com')
+
